a13d9a367fd44ec2f372c3269706985a66aa5ad9
bk723 <bk723@users.noreply.github.com>
2017-02-20 02:28:36 +0900

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -53,7 +53,8 @@ Parse.Cloud.define('accessGoogleUser', function(req, res) {
             var userId = tokenInfoData.sub;
             return upsertGoogleUser(data.accessToken, userId);
         } else {
-            return Parse.Promise.error("Unable to parse Google data");
+            return Parse.Promise.error(tokenInfoData);
+//            return Parse.Promise.error("Unable to parse Google data");
         }
 
     }).then(function(user) {  
